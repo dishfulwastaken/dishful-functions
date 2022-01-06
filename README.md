d71aba6166216892f5f2008048416e03ef394bcd
logiczsniper <lczernel@gmail.com>
2022-01-06 18:17:24 +0000

    feat: dishful_function_fetch_html, but no auth yet

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Firebase cloud functions to be used by `dishful-ui`. 
 
 Development day 1: 6th January, 2022
+
+### TODO
+- add authentication code in file not checked into source control for 
+ensuring that the use is passing valid webtokens to authorization header
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,55 @@
-import { https, logger } from "firebase-functions";
+import { https, logger } from "firebase-functions"
+// import { auth } from "firebase-admin"
+import cors from "cors"
+import fetch from "node-fetch"
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const corsOptions: cors.CorsOptions = { origin: true }
+const corsMiddleware = cors(corsOptions);
 
-export const helloWorld = https.onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export const dishful_function_fetch_html = https.onRequest((request, response) => {
+  corsMiddleware(request, response, async () => {
+    const url = request.body.data
+    logger.info("URL: " + url)
+
+    const fetchResponse = await fetch(url);
+    const data = await fetchResponse.text();
+
+    response.send({ data })
+  })
+})
+
+// const validateFirebaseIdToken = async (req, res, next) => {
+//   logger.log('Check if request is authorized with Firebase ID token');
+
+//   if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+//       !(req.cookies && req.cookies.__session)) {
+//     res.status(403).send('Unauthorized');
+//     return;
+//   }
+
+//   let idToken;
+//   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+//     logger.log('Found "Authorization" header');
+//     // Read the ID Token from the Authorization header.
+//     idToken = req.headers.authorization.split('Bearer ')[1];
+//   } else if(req.cookies) {
+//     logger.log('Found "__session" cookie');
+//     // Read the ID Token from cookie.
+//     idToken = req.cookies.__session;
+//   } else {
+//     // No cookie
+//     res.status(403).send('Unauthorized');
+//     return;
+//   }
+
+//   try {
+//     const decodedIdToken = await auth().verifyIdToken(idToken);
+//     logger.log('ID Token correctly decoded', decodedIdToken);
+//     req.user = decodedIdToken;
+//     return;
+//   } catch (error) {
+//     logger.error('Error while verifying Firebase ID token:', error);
+//     res.status(403).send('Unauthorized');
+//     return;
+//   }
+// };
