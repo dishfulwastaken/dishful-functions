0d0088d0c85e5a6ea1e0dacba9aa5565c78b7f6b
logiczsniper <lczernel@gmail.com>
2022-01-14 11:03:27 +0000

    fix(onRecipeIteration): collection paths now nested in user collection

--- functions/src/onRecipeIterations.ts
+++ functions/src/onRecipeIterations.ts
@@ -1,20 +1,26 @@
 import { firestore as functionsFirestore } from "firebase-functions"
 import { firestore as adminFirestore } from "firebase-admin"
 
-const db = adminFirestore()
-const recipeIterationsPath = "dishful_firebase_storage_recipe_meta/{recipeMetaId}/dishful_firebase_storage_recipe_iteration/{recipeIterationId}"
+enum CollectionName {
+  User = "dishful_firebase_storage_user_meta",
+  Recipe = "dishful_firebase_storage_recipe_meta",
+  RecipeIteration = "dishful_firebase_storage_recipe_iteration",
+}
 
-const updateRecipeIterationCount = ({recipeMetaId, increment}: {recipeMetaId: any, increment: 1 | -1}) => {
-  const recipeMetaRef = db.collection("dishful_firebase_storage_recipe_meta").doc(recipeMetaId)
+const db = adminFirestore()
+const recipeIterationsPath = `${CollectionName.User}/{userId}/${CollectionName.Recipe}/{recipeMetaId}/${CollectionName.RecipeIteration}/{recipeIterationId}`
+
+const updateRecipeIterationCount = ({userId, recipeMetaId, increment}: {userId: any, recipeMetaId: any, increment: 1 | -1}) => {
+  const recipeMetaRef = db.collection(`${CollectionName.User}/${userId}/${CollectionName.Recipe}`).doc(recipeMetaId)
   recipeMetaRef.update({iterationCount: adminFirestore.FieldValue.increment(increment)})
 }
 
 export const onRecipeIterationsCreate = functionsFirestore.document(recipeIterationsPath).onCreate((_, context) => {
-  const recipeMetaId = context.params.recipeMetaId
-  updateRecipeIterationCount({ recipeMetaId, increment: 1 })
+  const { userId, recipeMetaId } = context.params
+  updateRecipeIterationCount({ userId, recipeMetaId, increment: 1 })
 })
 
 export const onRecipeIterationsDelete = functionsFirestore.document(recipeIterationsPath).onDelete((_, context) => {
-  const recipeMetaId = context.params.recipeMetaId
-  updateRecipeIterationCount({ recipeMetaId, increment: -1 })
+  const { userId, recipeMetaId } = context.params
+  updateRecipeIterationCount({ userId, recipeMetaId, increment: -1 })
 })
