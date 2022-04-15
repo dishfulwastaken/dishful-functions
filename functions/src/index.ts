753050c9d663f3f1bb4d5f84ecca2e2e53047c30
logiczsniper <lczernel@gmail.com>
2022-04-15 20:22:51 +0100

    refactor: simplify backend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,3 @@
 import { fetchHtml } from "./fetchHtml"
-import { onRecipeIterationsCreate, onRecipeIterationsDelete } from "./onRecipeIterations"
 
 export const dishful_function_fetch_html = fetchHtml
-export const dishful_function_on_recipe_iterations_create = onRecipeIterationsCreate
-export const dishful_function_on_recipe_iterations_delete = onRecipeIterationsDelete
